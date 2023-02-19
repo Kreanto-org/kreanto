37fbaabae55edf45938b764a7ddc499d323d86c6
SantiagoVira <santiagovira06@gmail.com>
2023-02-19 17:44:37 -0500

    Finish prisma

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,36 +1,9 @@
 import type { GetServerSidePropsContext } from "next";
-import {
-  getServerSession,
-  type NextAuthOptions,
-  type DefaultSession,
-} from "next-auth";
+import { getServerSession, type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
-import { type User as PrismaUser } from "@prisma/client";
-
-/**
- * Module augmentation for `next-auth` types.
- * Allows us to add custom properties to the `session` object and keep type
- * safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- **/
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & PrismaUser;
-  }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
-}
 
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks,
@@ -45,7 +18,12 @@ export const authOptions: NextAuthOptions = {
         session.user.id = user.id;
         session.user.age = user.age;
         session.user.lastActive = user.lastActive;
-        // session.user.role = user.role; <-- put other properties on the session here
+        session.user.avgRespTime = user.avgRespTime;
+        session.user.name = user.name;
+        session.user.location = user.location;
+        session.user.avgRespTime = user.avgRespTime;
+        session.user.numResponses = user.numResponses;
+        session.user.printerProfile = user.printerProfile;
       }
       return session;
     },
