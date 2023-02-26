166634b8d9d1751c17487ef7bb4b7b9562b7b9de
SantiagoVira <santiagovira06@gmail.com>
2023-02-26 16:11:59 -0500

    improve tooltip and add more

--- src/components/page-specific/profile/printer-info-section.tsx
+++ src/components/page-specific/profile/printer-info-section.tsx
@@ -24,20 +24,32 @@ const PrinterInfoSection: React.FC<{ profile: PrinterProfile }> = ({
       <div className="flex items-center gap-2">
         <GiNestedHexagons />
         <p>{profile.points} points</p>
+        <InfoTooltip>
+          This tells you how experienced this printer is.
+        </InfoTooltip>
       </div>
       <div className="flex items-center gap-2">
         <BiTimeFive /> <p>{printTimeWords[profile.printTime]} availability</p>
+        <InfoTooltip>
+          This tells you how available this person is to print something.
+        </InfoTooltip>
       </div>
       <div className="flex items-center gap-2">
         <MdOutlineColorLens />{" "}
         <p className="capitalize">{colorTypeWords[profile.colorType]} color</p>
+        <InfoTooltip>
+          This tells you how many colors this person can use in a single print.
+        </InfoTooltip>
       </div>
       <div className="flex items-center gap-2">
         <BiCube />{" "}
         <p>
           {profile.length}mm x {profile.width}mm x {profile.height}mm
         </p>
-        <InfoTooltip message="These are the dimensions of the 3d printer this person uses, telling you how big they can print!" />
+        <InfoTooltip>
+          These are the dimensions of the 3d printer this person uses, telling
+          you how big they can print!
+        </InfoTooltip>
       </div>
     </div>
   );
--- src/components/ui/info-tooltip.tsx
+++ src/components/ui/info-tooltip.tsx
@@ -1,15 +1,15 @@
 import { AiOutlineQuestionCircle } from "react-icons/ai";
 
-const InfoTooltip: React.FC<{ message: string }> = ({ message }) => {
+const InfoTooltip: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <div className="group relative flex">
       <AiOutlineQuestionCircle />
       <span
-        className="absolute left-1/2 m-4 mx-auto w-[15rem] -translate-x-1/2 translate-y-[10px] rounded-md 
-        bg-gray-800 py-2 px-3 text-sm text-gray-100 
-        opacity-0 transition-opacity group-hover:opacity-100"
+        className="transition-display absolute left-1/2 m-4 mx-auto hidden w-[15rem] -translate-x-1/2 
+        translate-y-[10px] rounded-md bg-gray-800 py-2 px-3 
+        text-sm text-gray-100 group-hover:block"
       >
-        {message}
+        {children}
       </span>
     </div>
   );
