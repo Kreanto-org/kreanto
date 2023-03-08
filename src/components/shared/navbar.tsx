9d43f89d273f420631d79cc41ca0efc3a4fd93e9
SantiagoVira <santiagovira06@gmail.com>
2023-03-07 20:20:10 -0500

    fix navbard centering

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -11,14 +11,14 @@ const Navbar: React.FC = () => {
 
   return (
     <div className="flex h-[50px] w-screen items-center justify-between bg-bg-200 px-20 shadow">
-      <Link href="/">
+      <Link href="/" className="flex-1">
         <Image src="/White Logo.png" alt="" width={30} height={10} />
       </Link>
       {mainTabs.map((tab, i) => (
         <Tab key={i} {...tab} />
       ))}
       <button
-        className="text-highlight no-underline transition hover:-translate-y-0.5"
+        className="flex-1 text-right text-highlight no-underline transition hover:-translate-y-0.5"
         onClick={
           sessionData
             ? () => void signOut()
