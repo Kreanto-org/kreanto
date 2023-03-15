b7a9e63dfa6f9c0177ca195926a0960357a0555e
SantiagoVira <santiagovira06@gmail.com>
2023-03-14 22:27:58 -0400

    3d stuff?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { Canvas } from "@react-three/fiber";
 import { type NextPage } from "next";
+import Box from "~/components/box";
 import Layout from "~/components/shared/layout";
 
 const Home: NextPage = () => {
@@ -10,6 +12,11 @@ const Home: NextPage = () => {
       <p className="mt-4 text-text-200">
         Enabling creators, empowering makers.
       </p>
+      <Canvas>
+        <Box />
+        <ambientLight intensity={0.1} />
+        <directionalLight />
+      </Canvas>
     </Layout>
   );
 };
