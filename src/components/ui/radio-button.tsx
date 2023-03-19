7a64f4f5332d5269f7676a2740afcee2de120666
SantiagoVira <santiagovira06@gmail.com>
2023-03-19 02:12:11 -0400

    fix colors

--- src/components/ui/radio-button.tsx
+++ src/components/ui/radio-button.tsx
@@ -30,11 +30,11 @@ const RadioButton: React.FC<RadioButtonProps> = ({
       />
       <div
         className={cn(
-          `flex flex-col items-center border-2 border-x-[1px] border-text-300 p-3 text-text-200 peer-checked:border-text-100 peer-checked:text-text-100`,
+          `flex flex-col items-center border-2 border-x-[1px] border-text-300 p-3 text-text-200 peer-checked:border-text-100/70 peer-checked:text-text-100`,
           className,
           left ? "rounded-l-lg border-l-2" : "",
           right ? "rounded-r-lg border-r-2" : "",
-          noBg ? "" : "peer-checked:bg-white/10"
+          noBg ? "" : "peer-checked:bg-black/10"
         )}
         {...rest}
       >
