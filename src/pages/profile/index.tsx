3f6d23de6f5e78d27f1e8fdacbe9b003055bd0c2
SantiagoVira <santiagovira06@gmail.com>
2023-03-23 22:23:37 -0400

    profile responsiveness

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,7 +2,6 @@ import { useSession } from "next-auth/react";
 import PrinterInfoSection from "~/components/page-specific/profile/printer-info-section";
 import ResponseInfoSection from "~/components/page-specific/profile/response-info-section";
 import Layout from "~/components/shared/layout";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
 const Profile: React.FC = () => {
   const { data: sessionData } = useSession();
@@ -14,28 +13,13 @@ const Profile: React.FC = () => {
       needsAuth
       className="items-start"
     >
-      <div className="mx-4 flex flex-col items-start gap-1  px-6 pt-2 pb-4">
-        <div className="flex w-full items-center gap-4">
-          <Avatar>
-            <AvatarImage
-              src={sessionData?.user.image ?? undefined}
-              alt="user-pfp"
-            />
-            <AvatarFallback>
-              {sessionData?.user.name
-                ?.split(" ")
-                .map((name) => name[0])
-                .join("")
-                .toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-          <h1 className="w-full text-left ">{sessionData?.user.name}</h1>
-        </div>
+      <div className="mx-2 flex flex-col items-start gap-1  px-6 pt-2 pb-4">
+        <h1 className="w-full text-left">{sessionData?.user.name}</h1>
 
         <ResponseInfoSection {...sessionData?.user} />
       </div>
       {sessionData?.user.printerProfile && (
-        <div className="mx-4 px-6 py-4">
+        <div className="mx-8 px-6 pb-2">
           <PrinterInfoSection profile={sessionData?.user.printerProfile} />
         </div>
       )}
