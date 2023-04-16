4f4604b62ef0099b300ac7e77ef23a9e6ea0ca39
SantiagoVira <santiagovira06@gmail.com>
2023-04-15 22:21:08 -0400

    inifinite scroll

--- src/pages/messages/[id].tsx
+++ src/pages/messages/[id].tsx
@@ -1,9 +1,11 @@
+import { Message } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Layout from "~/components/shared/layout";
 import Button from "~/components/ui/button";
 import { api } from "~/utils/api";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const MessageChat: React.FC = () => {
   const router = useRouter();
@@ -13,8 +15,37 @@ const MessageChat: React.FC = () => {
   const canAccess = canAccessQuery.data;
 
   const sendMut = api.message.send.useMutation();
-  const messagesQuery = api.message.getMessages.useQuery({ chatId: id });
-  const messages = messagesQuery.data;
+  const messagesQuery = api.message.infiniteQuery.useInfiniteQuery(
+    { chatId: id, limit: 30 },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+      // initialCursor: 1, // <-- optional you can pass an initialCursor
+    }
+  );
+
+  // ----------------------
+
+  const { data, fetchNextPage } = api.message.infiniteQuery.useInfiniteQuery(
+    {
+      limit: 3,
+      chatId: id,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  const paginatedData: Message[] = [];
+  data?.pages?.forEach((page) => {
+    page.items?.forEach((char) => {
+      // paginatedData.splice(0, 0, char);
+      paginatedData.push(char);
+    });
+  });
+
+  // ----------
+
+  const messages = messagesQuery.data?.pages;
   const [message, setMessage] = useState("");
 
   if (!canAccess && status !== "loading" && canAccessQuery.status !== "loading")
@@ -36,9 +67,26 @@ const MessageChat: React.FC = () => {
       >
         Send
       </Button>
-      {messages?.map((m, i) => (
-        <p key={i}>{m.text}</p>
-      ))}
+      <div
+        id="scrollableDiv"
+        className="flex h-[300px] w-full flex-col-reverse overflow-auto"
+      >
+        <InfiniteScroll
+          dataLength={paginatedData.length}
+          next={fetchNextPage}
+          style={{ display: "flex", flexDirection: "column-reverse" }} //To put endMessage and loader to the top.
+          inverse={true} //
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          scrollableTarget="scrollableDiv"
+        >
+          {paginatedData?.map((m, i) => (
+            <p key={i} onScroll={() => console.log("yo")} className="mt-24">
+              {m.text}
+            </p>
+          ))}
+        </InfiniteScroll>
+      </div>
     </Layout>
   );
 };
--- src/server/api/routers/message.ts
+++ src/server/api/routers/message.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { canAccess } from "~/utils/trpcHelpers";
@@ -37,5 +38,37 @@ export const messageRouter = createTRPCRouter({
         where: { chatId: input.chatId },
         orderBy: { sentOn: "asc" },
       });
+    }),
+
+  infiniteQuery: protectedProcedure
+    .input(
+      z.object({
+        chatId: z.string(),
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.date().nullish(), // <-- "cursor" needs to exist, but can be any type
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const limit = input.limit ?? 50;
+      const { cursor } = input;
+      const items = await ctx.prisma.message.findMany({
+        take: limit + 1, // get an extra item at the end which we'll use as next cursor
+        where: { chatId: input.chatId },
+        cursor: cursor
+          ? { chatId_sentOn: { chatId: input.chatId, sentOn: cursor } }
+          : undefined,
+        orderBy: {
+          sentOn: "desc",
+        },
+      });
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (items.length > limit) {
+        const nextItem = items.pop();
+        nextCursor = nextItem!.sentOn;
+      }
+      return {
+        items,
+        nextCursor,
+      };
     }),
 });
