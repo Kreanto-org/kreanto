dfda94e63443c0dabf95df4ea1127eba9217c523
SantiagoVira <santiagovira06@gmail.com>
2023-05-16 18:16:40 -0400

    404 next page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,14 @@
+import type { NextPage } from "next";
 import Layout from "~/components/shared/layout";
 
-const Error404: React.FC = () => {
-  return (
-    <Layout title="404">
-      <h1>
-        4<span className="text-highlight">0</span>4
-      </h1>
-      <p>The page you are looking for does not exist!</p>
-      <p>Check that your url is correct.</p>
-    </Layout>
-  );
-};
+const Error404: NextPage = () => (
+  <Layout title="404">
+    <h1>
+      4<span className="text-highlight">0</span>4
+    </h1>
+    <p>The page you are looking for does not exist!</p>
+    <p>Check that your url is correct.</p>
+  </Layout>
+);
 
 export default Error404;
