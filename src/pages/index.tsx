9a2655c70d4bb86afb2d1bd4b39b2f1e9cf2c78f
SantiagoVira <santiagovira06@gmail.com>
2023-05-23 12:24:34 -0400

    indecisive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import Layout from "~/components/shared/layout";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1 className="mt-10 tracking-tight text-white sm:text-[5rem]">
+      {/* <h1 className="mt-10 tracking-tight text-white sm:text-[5rem]">
         KREANTO
       </h1>
       <p className="mt-4 text-text-200">
         Enabling creators, empowering makers.
-      </p>
+      </p> */}
     </Layout>
   );
 };
